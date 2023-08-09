6b0c4e83e1f59036b4fdd18c99e0d0cb26285276
tracer-nju <tracer@nju.edu.cn>
2023-08-09 20:34:20 +0800

    Linux niujh-Jiaolong-Series-GK5NP5O 5.15.0-79-generic #86~20.04.2-Ubuntu SMP Mon Jul 17 23:27:17 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:34:20 up 34 min,  1 user,  load average: 0.99, 0.82, 0.70

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <assert.h>
 
-int main(int argc, char *argv[]) {
-  for (int i = 0; i < argc; i++) {
+int main(int argc, char *argv[])
+{
+  printf("argc=%d \n", argc);
+  for (int i = 0; i < argc; i++)
+  {
     assert(argv[i]);
     printf("argv[%d] = %s\n", i, argv[i]);
   }
