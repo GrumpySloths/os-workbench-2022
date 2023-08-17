8cf4cfe224af79c52a080dfcb9afa602596a8bda
tracer-nju <tracer@nju.edu.cn>
2023-08-17 20:42:11 +0800

    Linux niujh-Jiaolong-Series-GK5NP5O 5.15.0-79-generic #86~20.04.2-Ubuntu SMP Mon Jul 17 23:27:17 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:42:11 up  2:22,  1 user,  load average: 0.49, 0.52, 0.51

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -2,6 +2,8 @@
 #include <assert.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <dirent.h>
+#include <stdlib.h>
 int main(int argc, char *argv[])
 {
   printf("argc=%d \n", argc);
@@ -13,7 +15,34 @@ int main(int argc, char *argv[])
   printf("hello world\n");
   // printf("hello world");
   assert(!argv[argc]);
-  pid_t pid = getpid();
-  printf("pid:%d \n", pid);
+  char *folderPath = "/proc/";
+  listSubfolders(folderPath);
+  // pid_t pid = getpid();
+  // printf("pid:%d \n", pid);
+
   return 0;
+}
+
+void listSubfolders(const char *folderPath)
+{
+  DIR *dir = opendir(folderPath);
+  if (dir == NULL)
+  {
+    perror("Error opening folder");
+    return;
+  }
+
+  struct dirent *entry;
+  while ((entry = readdir(dir)) != NULL)
+  {
+    if (entry->d_type == DT_DIR)
+    { // Check if it's a directory
+      if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0)
+      {
+        printf("%s\n", entry->d_name);
+      }
+    }
+  }
+
+  closedir(dir);
 }
