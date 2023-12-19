2ed52b6fabf2cb8614a82a90c35ef2de7dda05f6
tracer-nju <tracer@nju.edu.cn>
2023-12-19 11:36:03 +0800

    Linux niujh-Jiaolong-Series-GK5NP5O 5.15.0-91-generic #101~20.04.1-Ubuntu SMP Thu Nov 16 14:22:28 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:36:03 up  3:15,  1 user,  load average: 1.84, 1.32, 1.01

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -3,7 +3,7 @@
 // Operating system is a C program!
 int main(const char *args) {
   ioe_init();
-  printf("print test %s\n","hello world");
+  printf("print test %s %d\n","hello world",5);
   puts("mainargs = \"");
   puts(args); // make run mainargs=xxx
   puts("\"\n");
