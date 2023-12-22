ec28bcb8a7a8df171c5a14108220a0f8be627787
tracer-nju <tracer@nju.edu.cn>
2023-12-22 11:09:13 +0800

    Linux niujh-Jiaolong-Series-GK5NP5O 5.15.0-91-generic #101~20.04.1-Ubuntu SMP Thu Nov 16 14:22:28 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:09:13 up  2:08,  1 user,  load average: 1.04, 0.91, 0.75

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -1,17 +1,22 @@
 #include <game.h>
 
+typedef struct{
+    int x;
+    int y;
+} Square; //定义小方块对象
 // Operating system is a C program!
 int main(const char *args) {
-  ioe_init();
-  puts("mainargs = \"");
-  puts(args); // make run mainargs=xxx
-  puts("\"\n");
+    ioe_init();
+    puts("mainargs = \"");
+    puts(args);  // make run mainargs=xxx
+    puts("\"\n");
 
-
-  puts("Press any key to see its key code...\n");
-  while (1) {
-    splash();
-    print_key();
-  }
-  return 0;
+    Square square=(Square){.x=10,.y=10};
+    printf("squre.x:%d\n", square.x);
+    puts("Press any key to see its key code...\n");
+    while (1) {
+        splash();
+        print_key();
+    }
+    return 0;
 }
