2ff8b63f35e3ec3a4941059638c025d3f40ca4b1
tracer-nju <tracer@nju.edu.cn>
2023-12-24 17:06:16 +0800

    Linux niujh-Jiaolong-Series-GK5NP5O 5.15.0-91-generic #101~20.04.1-Ubuntu SMP Thu Nov 16 14:22:28 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:06:16 up  2:32,  1 user,  load average: 1.59, 1.44, 1.10

--- amgame/src/video.c
+++ amgame/src/video.c
@@ -42,5 +42,5 @@ void flush() {
     // printf("flush is called\n");
     init();
     printf("w=%d,h=%d\n", w, h);
-    draw_tile(0, 0, 120, 120, 0xffffff);
+    draw_tile(0, 0, 64, 64, 0xffffff);
 }
