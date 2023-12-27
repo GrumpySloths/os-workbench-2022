74751b749093c15f45299f5224961a391fd3f52c
tracer-nju <tracer@nju.edu.cn>
2023-12-27 10:06:29 +0800

    Linux niujh-Jiaolong-Series-GK5NP5O 5.15.0-91-generic #101~20.04.1-Ubuntu SMP Thu Nov 16 14:22:28 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:06:29 up  1:06,  1 user,  load average: 0.29, 0.53, 0.69

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -10,7 +10,6 @@ static void kfree(void *ptr) {
 static void pmm_init() {
   uintptr_t pmsize = ((uintptr_t)heap.end - (uintptr_t)heap.start);
   printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, heap.start, heap.end);
-  // printf("Got %d MiB heap: [%d, %d)\n", pmsize >> 20, heap.start, heap.end);
 
 }
 
