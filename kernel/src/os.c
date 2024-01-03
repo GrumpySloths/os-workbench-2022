509115a0f2d02ec290dae74ca1336af2b8021881
tracer-nju <tracer@nju.edu.cn>
2024-01-03 10:56:49 +0800

    Linux niujh-Jiaolong-Series-GK5NP5O 5.15.0-91-generic #101~20.04.1-Ubuntu SMP Thu Nov 16 14:22:28 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:56:49 up  2:15,  1 user,  load average: 1.35, 0.89, 0.70

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -1,14 +1,35 @@
 #include <common.h>
-
-static void os_init() {
-  pmm->init();
-}
+// enum ops { OP_ALLOC = 1, OP_FREE };
+// struct malloc_op {
+//   enum ops type;
+//   union { size_t sz; void *addr; };
+// };
+// //返回 0-1之间的随机数
+// static float randn() { return (float)rand() / 32768; }
+// static struct malloc_op random_op(){
+//   if(randn()<0.5){
+//       struct malloc_op op = (struct malloc_op){.type = OP_ALLOC, .sz = rand()};
+//       return op;
+//   }else{
+//       struct malloc_op op = (struct malloc_op){.type = OP_FREE, .sz = rand()};
+//   }
+// }
+typedef struct{
+    int x;
+    int y;
+} s1_t;
+static void os_init() { pmm->init(); }
 
 static void os_run() {
-  for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
-    putch(*s == '*' ? '0' + cpu_current() : *s);
-  }
-  while (1) ;
+    s1_t* s1 = (s1_t*)pmm->alloc(sizeof(s1_t));
+    s1->x = 10;
+    s1->y = 2;
+    printf("s1: x=%d,y=%d\n", s1->x, s1->y);
+    pmm->free(s1);
+    // for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
+    //   putch(*s == '*' ? '0' + cpu_current() : *s);
+    // }
+    // while (1) ;
 }
 
 MODULE_DEF(os) = {
