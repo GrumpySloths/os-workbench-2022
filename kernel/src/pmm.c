f0ab6640247fd9dd0330addfd6a81de2d056e292
tracer-nju <tracer@nju.edu.cn>
2024-01-03 09:46:26 +0800

    Linux niujh-Jiaolong-Series-GK5NP5O 5.15.0-91-generic #101~20.04.1-Ubuntu SMP Thu Nov 16 14:22:28 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:46:26 up  1:05,  1 user,  load average: 0.75, 0.73, 0.60

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -1,18 +1,26 @@
 #include <common.h>
 
 static void *kalloc(size_t size) {
-  return NULL;
+    return malloc(size);
 }
 
-static void kfree(void *ptr) {
-}
-
+static void kfree(void *ptr) { free(ptr); }
+#ifndef TEST
 static void pmm_init() {
   uintptr_t pmsize = ((uintptr_t)heap.end - (uintptr_t)heap.start);
+  
   printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, heap.start, heap.end);
 
 }
-
+#else
+// 测试代码的 pmm_init ()
+static void pmm_init() {
+  char *ptr  = malloc(HEAP_SIZE);
+  heap.start = ptr;
+  heap.end   = ptr + HEAP_SIZE;
+  printf("Got %d MiB heap: [%p, %p)\n", HEAP_SIZE >> 20, heap.start, heap.end);
+}
+#endif
 MODULE_DEF(pmm) = {
   .init  = pmm_init,
   .alloc = kalloc,
---
+++ kernel/test/test.c
@@ -0,0 +1,8 @@
+static void entry(int tid) { pmm->alloc(128); }
+static void goodbye()      { printf("End.\n"); }
+int main() {
+  pmm->init();
+  for (int i = 0; i < 4; i++)
+    create(entry);
+  join(goodbye);
+}
