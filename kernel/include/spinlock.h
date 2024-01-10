1841d84d2d6130e6a2b851e025c0a8aa8c82cab3
tracer-nju <tracer@nju.edu.cn>
2024-01-10 11:05:28 +0800

    Linux niujh-Jiaolong-Series-GK5NP5O 5.15.0-91-generic #101~20.04.1-Ubuntu SMP Thu Nov 16 14:22:28 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:05:28 up  2:15,  1 user,  load average: 0.95, 0.93, 0.79

--- kernel/include/spinlock.h
+++ kernel/include/spinlock.h
@@ -1,15 +1,10 @@
+#ifndef SPINLOCK_H__
+#define SPINLOCK_H__
 #include <stdint.h>
 // Spinlock
 typedef int spinlock_t;
 #define SPIN_INIT() 0
+void spin_lock(spinlock_t *);
+void spin_unlock(spinlock_t *);
 
-
-void spin_lock(spinlock_t *lk) {
-    while (1) {
-        intptr_t value = atomic_xchg(lk, 1);
-        if (value == 0) {
-            break;
-        }
-    }
-}
-void spin_unlock(spinlock_t *lk) { atomic_xchg(lk, 0); }
+#endif
