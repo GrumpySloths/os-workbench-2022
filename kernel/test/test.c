a010bae6cf33f6e7f1834e6de438024c146d6cd4
tracer-nju <tracer@nju.edu.cn>
2024-01-16 10:06:28 +0800

    Linux niujh-Jiaolong-Series-GK5NP5O 5.15.0-91-generic #101~20.04.1-Ubuntu SMP Thu Nov 16 14:22:28 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:06:28 up  1:14,  1 user,  load average: 0.81, 0.88, 0.78

--- kernel/test/test.c
+++ kernel/test/test.c
@@ -1,8 +1,8 @@
 #include<common.h>
 #include<thread.h>
 #include<stdio.h>
-static void entry(int tid) { malloc(128); }
-// static void entry(int tid) { pmm->alloc(128); }
+// static void entry(int tid) { malloc(128); }
+static void entry(int tid) { pmm->alloc(128); }
 static void goodbye()      { printf("End.\n"); }
 int main() {
     printf("test\n");
