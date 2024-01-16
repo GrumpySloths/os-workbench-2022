cf2d31b3a9a93df15ab8a7d6281d0bab52d8625a
tracer-nju <tracer@nju.edu.cn>
2024-01-16 10:06:41 +0800

    Linux niujh-Jiaolong-Series-GK5NP5O 5.15.0-91-generic #101~20.04.1-Ubuntu SMP Thu Nov 16 14:22:28 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:06:41 up  1:15,  1 user,  load average: 0.77, 0.86, 0.78

--- kernel/test/test.c
+++ kernel/test/test.c
@@ -6,7 +6,7 @@ static void entry(int tid) { pmm->alloc(128); }
 static void goodbye()      { printf("End.\n"); }
 int main() {
     printf("test\n");
-    // pmm->init();
+    pmm->init();
     for (int i = 0; i < 4; i++) create(entry);
     join(goodbye);
 }
