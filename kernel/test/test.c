c281adc93164bd9fcb2f1d937b7a02446b649205
tracer-nju <tracer@nju.edu.cn>
2024-01-20 10:17:33 +0800

    Linux niujh-Jiaolong-Series-GK5NP5O 5.15.0-91-generic #101~20.04.1-Ubuntu SMP Thu Nov 16 14:22:28 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:17:33 up 54 min,  1 user,  load average: 1.45, 1.23, 0.86

--- kernel/test/test.c
+++ kernel/test/test.c
@@ -1,5 +1,9 @@
 #include<common.h>
 #include<stdio.h>
+#ifdef TEST  
+#define malloc(s) mymalloc(s)
+#define free(pt) myfree(pt)
+#endif
 // static void entry(int tid) { malloc(128);
 //     printf("alloc success\n");
 // }
