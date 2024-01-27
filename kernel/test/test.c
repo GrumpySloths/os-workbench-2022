e440313b09bb4aff7ce4f73e031a209a86e9aef4
tracer-nju <tracer@nju.edu.cn>
2024-01-27 11:38:25 +0800

    Linux niujh-Jiaolong-Series-GK5NP5O 5.15.0-94-generic #104~20.04.1-Ubuntu SMP Tue Jan 16 13:34:09 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:38:25 up  3:25,  1 user,  load average: 1.03, 0.61, 0.53

--- kernel/test/test.c
+++ kernel/test/test.c
@@ -6,10 +6,17 @@
 
 static int idx = 0;
 
+static void entry_0(int tid){
+    while(1){
+        pmm->alloc(64);
+    }
+}
 //频繁地小内存释放测试，绝大多数的小于128b,
 static void do_test_0(){
     pmm->init();
-
+    for (int i = 0; i < 4;i++){
+        create(entry_0);
+    }
 }
 
 static void entry(int tid) {
@@ -35,11 +42,12 @@ static void entry(int tid) {
 static void goodbye() { printf("End.\n"); }
 
 int main() {
-    printf("test\n");
-    pmm->init();
-    for (int i = 0; i < 8; i++) create(entry);
-    // join(goodbye);
-    void* pt = pmm->alloc(32);
-    pmm->free(pt);
-    printf("program terminal\n");
+    do_test_0();
+    // printf("test\n");
+    // pmm->init();
+    // for (int i = 0; i < 4; i++) create(entry);
+    // // join(goodbye);
+    // void* pt = pmm->alloc(32);
+    // pmm->free(pt);
+    // printf("program terminal\n");
 }
