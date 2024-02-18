7e3c6d124843d4eca59adb2bad1b027a5278f85e
tracer-nju <tracer@nju.edu.cn>
2024-02-18 09:32:07 +0800

    Linux niujh-Jiaolong-Series-GK5NP5O 5.15.0-94-generic #104~20.04.1-Ubuntu SMP Tue Jan 16 13:34:09 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:32:07 up 53 min,  1 user,  load average: 0.93, 0.81, 0.80

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -3,21 +3,11 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-    for (int i = 0; i < argc;i++){
-        printf("%s\n", argv[i]);
-    }
-        char *exec_argv[] = {
-            "strace",
-            "ls",
-            NULL,
-        };
-    char *exec_envp[] = {
-        "PATH=/bin",
-        NULL,
-    };
-    execve("strace", exec_argv, exec_envp);
-    execve("/bin/strace", exec_argv, exec_envp);
-    execve("/usr/bin/strace", exec_argv, exec_envp);
-    perror(argv[0]);
-    exit(EXIT_FAILURE);
+  char *exec_argv[] = { "strace", "ls", NULL, };
+  char *exec_envp[] = { "PATH=/bin", NULL, };
+  execve("strace",          exec_argv, exec_envp);
+  // execve("/bin/strace",     exec_argv, exec_envp);
+  // execve("/usr/bin/strace", exec_argv, exec_envp);
+  perror(argv[0]);
+  exit(EXIT_FAILURE);
 }
