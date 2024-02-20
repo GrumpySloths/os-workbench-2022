679cb966527bd0e1dcf6fca8c34041116769d5eb
tracer-nju <tracer@nju.edu.cn>
2024-02-20 15:35:39 +0800

    Linux niujh-Jiaolong-Series-GK5NP5O 5.15.0-94-generic #104~20.04.1-Ubuntu SMP Tue Jan 16 13:34:09 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:35:39 up  6:47,  1 user,  load average: 0.93, 0.91, 0.74

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -9,41 +9,39 @@
 int main(int argc, char *argv[]) {
   char *exec_argv[] = { "strace","-T","ls", NULL, };
   char *exec_envp[] = {  "PATH=/bin",NULL, };
-  // int pipefd[2];
-  // pid_t cpid;
-  // char buf;
+  int pipefd[2];
+  pid_t cpid;
+  char buf;
 
-  // if (pipe(pipefd) == -1) {
-  //     perror("pipe");
-  //     exit(EXIT_FAILURE);
-  // }
+  if (pipe(pipefd) == -1) {
+      perror("pipe");
+      exit(EXIT_FAILURE);
+  }
 
-  // cpid = fork();
-  // if (cpid == -1) {
-  //     perror("fork");
-  //     exit(EXIT_FAILURE);
-  // }
+  cpid = fork();
+  if (cpid == -1) {
+      perror("fork");
+      exit(EXIT_FAILURE);
+  }
 
-  // if (cpid == 0) {    /* Child reads from pipe */
-  //     close(pipefd[1]);          /* Close unused write end */
+  if (cpid == 0) {    /* Child reads from pipe */
+      close(pipefd[1]);          /* Close unused write end */
 
-  //     while (read(pipefd[0], &buf, 1) > 0)
-  //         write(STDOUT_FILENO, &buf, 1);
+      while (read(pipefd[0], &buf, 1) > 0)
+          write(STDOUT_FILENO, &buf, 1);
 
-  //     write(STDOUT_FILENO, "\n", 1);
-  //     close(pipefd[0]);
-  //     _exit(EXIT_SUCCESS);
+      write(STDOUT_FILENO, "\n", 1);
+      close(pipefd[0]);
+      _exit(EXIT_SUCCESS);
 
-  // } else {            /* Parent writes argv[1] to pipe */
-  //     close(pipefd[0]);          /* Close unused read end */
-  //     write(pipefd[1], argv[1], strlen(argv[1]));
-  //     close(pipefd[1]);          /* Reader will see EOF */
-  //     wait(NULL);                /* Wait for child */
-  //     exit(EXIT_SUCCESS);
-  // }
+  } else {            /* Parent writes argv[1] to pipe */
+      close(pipefd[0]);          /* Close unused read end */
+      execve("/bin/strace",     exec_argv, exec_envp);
+      perror(argv[0]);
+      exit(EXIT_FAILURE);
+  }
   // execve("strace",          exec_argv, exec_envp);
-  execve("/bin/strace",     exec_argv, exec_envp);
+
   // execve("/usr/bin/strace", exec_argv, exec_envp);
-  perror(argv[0]);
-  exit(EXIT_FAILURE);
+
 }
