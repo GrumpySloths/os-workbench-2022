fd2950ffad3c1d13f252747f2f8f5fd8d99f62a9
tracer-nju <tracer@nju.edu.cn>
2024-02-20 15:41:24 +0800

    Linux niujh-Jiaolong-Series-GK5NP5O 5.15.0-94-generic #104~20.04.1-Ubuntu SMP Tue Jan 16 13:34:09 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:41:24 up  6:53,  1 user,  load average: 1.35, 1.09, 0.87

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -37,7 +37,8 @@ int main(int argc, char *argv[]) {
   } else {            /* child write pipe */
       close(pipefd[0]);          /* Close unused read end */
       printf("pipedf[1]:%d\n", pipefd[1]);
-      dup2(pipefd[1], 1);
+      // dup2(pipefd[1], 1);
+      close(1);
       execve("/bin/strace", exec_argv, exec_envp);
       perror(argv[0]);
       exit(EXIT_FAILURE);
