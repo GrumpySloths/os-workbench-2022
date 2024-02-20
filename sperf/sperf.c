7588d19394a2a810e5002aab5340026349167883
tracer-nju <tracer@nju.edu.cn>
2024-02-20 15:27:25 +0800

    Linux niujh-Jiaolong-Series-GK5NP5O 5.15.0-94-generic #104~20.04.1-Ubuntu SMP Tue Jan 16 13:34:09 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:27:25 up  6:39,  1 user,  load average: 0.59, 0.45, 0.52

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -1,10 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+#include<regex.h>
 
 int main(int argc, char *argv[]) {
-  char *exec_argv[] = { "strace", "ls", NULL, };
+  char *exec_argv[] = { "strace", "-T","ls", NULL, };
   char *exec_envp[] = {  "PATH=/bin",NULL, };
+  // int pipefd[2];
+  // pid_t cpid;
+  // char buf;
+
+  // if (pipe(pipefd) == -1) {
+  //     perror("pipe");
+  //     exit(EXIT_FAILURE);
+  // }
+
+  // cpid = fork();
+  // if (cpid == -1) {
+  //     perror("fork");
+  //     exit(EXIT_FAILURE);
+  // }
+
+  // if (cpid == 0) {    /* Child reads from pipe */
+  //     close(pipefd[1]);          /* Close unused write end */
+
+  //     while (read(pipefd[0], &buf, 1) > 0)
+  //         write(STDOUT_FILENO, &buf, 1);
+
+  //     write(STDOUT_FILENO, "\n", 1);
+  //     close(pipefd[0]);
+  //     _exit(EXIT_SUCCESS);
+
+  // } else {            /* Parent writes argv[1] to pipe */
+  //     close(pipefd[0]);          /* Close unused read end */
+  //     write(pipefd[1], argv[1], strlen(argv[1]));
+  //     close(pipefd[1]);          /* Reader will see EOF */
+  //     wait(NULL);                /* Wait for child */
+  //     exit(EXIT_SUCCESS);
+  // }
   // execve("strace",          exec_argv, exec_envp);
   execve("/bin/strace",     exec_argv, exec_envp);
   // execve("/usr/bin/strace", exec_argv, exec_envp);
