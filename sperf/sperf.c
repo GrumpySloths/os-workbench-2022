d110d829e773aa66a8e6dfe501d816961605488d
tracer-nju <tracer@nju.edu.cn>
2024-02-23 19:25:03 +0800

    Linux niujh-Jiaolong-Series-GK5NP5O 5.15.0-94-generic #104~20.04.1-Ubuntu SMP Tue Jan 16 13:34:09 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:25:03 up  3:43,  1 user,  load average: 1.16, 1.16, 0.97

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -241,7 +241,7 @@ int main(int argc, char *argv[]) {
           }
           if((time(NULL)-cur)>=1){
                 printf("\033[2;1H");
-                printf("\033[3J");
+                printf("\033[2J");
                 fflush(stdout);
                 cur = time(NULL);
                 find_top5(record, canvas);
