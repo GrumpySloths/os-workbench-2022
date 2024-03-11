eecff23c4aa826375290d503d3b07587365a1291
tracer-nju <tracer@nju.edu.cn>
2024-03-11 15:25:26 +0800

    Linux niujh-Jiaolong-Series-GK5NP5O 5.15.0-94-generic #104~20.04.1-Ubuntu SMP Tue Jan 16 13:34:09 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:25:26 up  6:38,  1 user,  load average: 1.57, 1.14, 1.06

--- kernel/test/am.h
+++ kernel/test/am.h
@@ -11,5 +11,17 @@ typedef struct {
 } Area;
 
 Area  heap;
+struct Context {
+  void    *cr3;
+  uint64_t rax, rbx, rcx, rdx,
+           rbp, rsi, rdi,
+           r8, r9, r10, r11,
+           r12, r13, r14, r15,
+           rip, cs, rflags,
+           rsp, ss, rsp0;
+};
+
+// Arch-dependent processor context
+typedef struct Context Context;
 
 #endif
