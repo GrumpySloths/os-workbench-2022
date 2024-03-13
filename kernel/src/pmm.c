8fba1df08496d7c5b9529a7bb9ffbc579bf5ff1c
tracer-nju <tracer@nju.edu.cn>
2024-03-13 11:39:34 +0800

    Linux niujh-Jiaolong-Series-GK5NP5O 5.15.0-94-generic #104~20.04.1-Ubuntu SMP Tue Jan 16 13:34:09 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:39:34 up  2:46,  1 user,  load average: 1.35, 1.00, 0.87

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -38,9 +38,10 @@ static void kfree_safe(void *ptr) {
 #ifndef TEST
 static void pmm_init() {
   uintptr_t pmsize = ((uintptr_t)heap.end - (uintptr_t)heap.start);
+  printf("pmsize:%d\n",pmsize);
   lk = 0;
   Log("cpu_count:%d",cpu_count());
-  printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, heap.start, heap.end);
+  Log("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, heap.start, heap.end);
 }
 #else
 // 测试代码的 pmm_init ()
