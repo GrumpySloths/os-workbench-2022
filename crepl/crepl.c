4a9091c6930eb4d3bc02953ec4a23bc7f95c5629
tracer-nju <tracer@nju.edu.cn>
2024-03-21 16:32:24 +0800

    Linux niujh-Jiaolong-Series-GK5NP5O 5.15.0-94-generic #104~20.04.1-Ubuntu SMP Tue Jan 16 13:34:09 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:32:24 up  2:10,  1 user,  load average: 0.82, 0.95, 0.90

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -20,7 +20,11 @@ int main(int argc, char *argv[]) {
     printf("template=%s\n",template);
     write(fd,line,strlen(line));
     close(fd);
-    // 通过system调用gcc编译这个文件
+    // 通过system调用gcc编译这个文件,使其变为.so文件
+    char cmd[4096];
+    snprintf(cmd, sizeof(cmd), "gcc -shared -fPIC %s -o %s.so", template, template);
+    printf("cmd=%s\n",cmd);
+    system(cmd);
     // 通过system调用./a.out执行这个文件
 
 
