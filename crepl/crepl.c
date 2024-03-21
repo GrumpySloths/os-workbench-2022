1c2d150f6e22644df35122b4efcf1633f35be69f
tracer-nju <tracer@nju.edu.cn>
2024-03-21 16:23:06 +0800

    Linux niujh-Jiaolong-Series-GK5NP5O 5.15.0-94-generic #104~20.04.1-Ubuntu SMP Tue Jan 16 13:34:09 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:23:06 up  2:01,  1 user,  load average: 1.50, 1.11, 0.90

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include<stdlib.h>
 #include<unistd.h>
+#include<assert.h>
 
 int main(int argc, char *argv[]) {
   static char line[4096];
@@ -14,6 +15,7 @@ int main(int argc, char *argv[]) {
     printf("Got %zu chars.\n", strlen(line)); // ??
     // 将line通过mkstemp api写入文件到/tmp目录下,后缀是.c
     int fd=mkstemps("/tmp/creplXXXXXX.c",2);
+    assert(fd>=0);
     write(fd,line,strlen(line));
     close(fd);
     // 通过system调用gcc编译这个文件
