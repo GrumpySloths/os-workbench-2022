f1d00b85e062e5f25183da94080aa76d90b463cd
tracer-nju <tracer@nju.edu.cn>
2024-03-22 16:01:29 +0800

    Linux niujh-Jiaolong-Series-GK5NP5O 5.15.0-94-generic #104~20.04.1-Ubuntu SMP Tue Jan 16 13:34:09 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:01:29 up  1:35,  1 user,  load average: 0.66, 0.55, 0.64

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -48,17 +48,19 @@ static void concurrency_test1(){
 
 #else
 static void tty_reader(void *arg) {
-  device_t *tty = dev->lookup(arg);
-  char cmd[128], resp[128], ps[16];
-  snprintf(ps, 16, "(%s) $ ", arg);
-  while (1) {
-    tty->ops->write(tty, 0, ps, strlen(ps));
-    printf("tty start reading\n");
-    int nread = tty->ops->read(tty, 0, cmd, sizeof(cmd) - 1);
-    cmd[nread] = '\0';
-    sprintf(resp, "tty reader task: got %d character(s).\n", strlen(cmd));
-    tty->ops->write(tty, 0, resp, strlen(resp));
-  }
+    TRACE_ENTRY;
+    device_t* tty = dev->lookup(arg);
+    char cmd[128], resp[128], ps[16];
+    snprintf(ps, 16, "(%s) $ ", arg);
+    while (1) {
+        tty->ops->write(tty, 0, ps, strlen(ps));
+        printf("tty start reading\n");
+        int nread = tty->ops->read(tty, 0, cmd, sizeof(cmd) - 1);
+        cmd[nread] = '\0';
+        sprintf(resp, "tty reader task: got %d character(s).\n", strlen(cmd));
+        tty->ops->write(tty, 0, resp, strlen(resp));
+    }
+    TRACE_EXIT;
 }
 
 #endif
