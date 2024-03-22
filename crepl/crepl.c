e96c534ad47eae5a61fdad87391d391ed96129e9
tracer-nju <tracer@nju.edu.cn>
2024-03-22 09:05:59 +0800

    Linux niujh-Jiaolong-Series-GK5NP5O 5.15.0-94-generic #104~20.04.1-Ubuntu SMP Tue Jan 16 13:34:09 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:05:59 up 47 min,  1 user,  load average: 0.65, 0.59, 0.61

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -42,7 +42,7 @@ int main(int argc, char *argv[]) {
       // 使用dlopen将.so文件加载到内存中
       //创建新的变量为template加上.so后缀
       strcat(template,".so");
-      handle = dlopen(template, RTLD_NOW);
+      handle = dlopen(template, RTLD_LAZY|RTLD_GLOBAL);
       if (handle == NULL) {
         fprintf(stderr, "%s\n", dlerror());
         continue;
