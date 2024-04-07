b9c3552429a53d72292ae03b1c5081df6a8d6104
wuhengknife <1273886989@qq.com>
2024-04-07 09:09:40 +0800

    分页机制实现前的版本保存

--- user/init.c
+++ user/init.c
@@ -1,7 +1,15 @@
 #include "ulib.h"
 
+#define putstr(s) \
+  ({ for (const char *p = s; *p; p++) kputc(*p); })
+
 int main() {
+
   // Example:
   // printf("pid = %d\n", getpid());
+  putstr("Hello, world from user mode!\n");
+
+  while(1);
+
   return 0;
 }
