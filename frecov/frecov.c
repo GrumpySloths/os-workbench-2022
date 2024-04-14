caf15112624e1c3852d9d1e409fbedecfa99ee21
tracer-nju <tracer@nju.edu.cn>
2024-04-14 15:33:58 +0800

    Linux niujh-Jiaolong-Series-GK5NP5O 5.15.0-94-generic #104~20.04.1-Ubuntu SMP Tue Jan 16 13:34:09 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:33:58 up 45 min,  1 user,  load average: 1.63, 1.27, 0.90

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -103,6 +103,7 @@ void *map_disk(const char *fname);
 void print_long_name(struct fat32longdir*longdir);
 struct fat32dir * get_RootDir(struct fat32hdr *hdr);
 struct fat32dir *ClusToDir(struct fat32hdr *hdr,int ClusId);
+u32 DirToClus(struct fat32dir*dir);
 static int EntCnt = 0;
 
 int main(int argc, char *argv[]) {
@@ -150,10 +151,7 @@ int main(int argc, char *argv[]) {
   printf("RootDir FstClusLO: %d\n", rootdir->DIR_FstClusLO);
   printf("RootDir FstClusHI: %d\n", rootdir->DIR_FstClusHI);
   //根据FstClusLO 和 FstClusHI 计算出下一个cluster的地址
-  u32 NextCluster = (rootdir->DIR_FstClusHI << 16) + rootdir->DIR_FstClusLO;
-  // u32 NextSector = ((NextCluster - 2) * hdr->BPB_SecPerClus) + FirstDataSector;
-  // u32 NextDirAddr = NextSector * hdr->BPB_BytsPerSec;
-  // struct fat32dir *nextdir = (struct fat32dir *)((char *)hdr + NextDirAddr);
+  u32 NextCluster = DirToClus(rootdir);
   struct fat32dir*nextdir=ClusToDir(hdr,NextCluster);
 
   //根据NextCluster计算对应的fat entry
@@ -297,4 +295,9 @@ struct fat32dir* ClusToDir(struct fat32hdr*hdr,int ClusId){
   struct fat32dir *nextdir = (struct fat32dir *)((char *)hdr + NextDirAddr);
 
   return nextdir;
+}
+
+u32 DirToClus(struct fat32dir*dir){
+  u32 ClusId = (dir->DIR_FstClusHI << 16) + dir->DIR_FstClusLO;
+  return ClusId;
 }
