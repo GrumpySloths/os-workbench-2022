61862525fc00eb4dbbdd46e30e2c5f4af025c30d
tracer-nju <tracer@nju.edu.cn>
2024-04-18 17:21:39 +0800

    Linux niujh-Jiaolong-Series-GK5NP5O 5.15.0-94-generic #104~20.04.1-Ubuntu SMP Tue Jan 16 13:34:09 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:21:39 up  2:51,  1 user,  load average: 1.39, 1.04, 0.88

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -381,7 +381,8 @@ void get_filename(fat32dir *dir, char *name) {
 }
 void FileSch(fat32hdr*hdr,fat32dir*dir,char*dirpath){
     //获取dir name，并添加.bmp后缀
-    if(dir->DIR_Name[0]=='.')
+    if(dir->DIR_Name[0]=='.'||dir->DIR_Name==0xe5
+        ||dir->DIR_Name==0x00)
         return;
     char name[30];
     int cluscnt = 0;
