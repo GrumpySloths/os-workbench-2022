0ab6c31c66ca5825aa4c4a1b46ceee96ed9aacba
wuhengknife <1273886989@qq.com>
2024-04-22 16:20:16 +0800

    准备用dfs实现整个文件系统的遍历

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -197,23 +197,7 @@ int main(int argc, char *argv[]) {
   printf("NextDir filesize: %d\n", nextdir->DIR_FileSize);
   struct fat32dir* temp = nextdir;
   
-  int cnt= 0;
   dfs(hdr, NextCluster, 0);
-  // for (; NextCluster < CLUS_INVALID;NextCluster=NextClus(hdr,NextCluster)){
-  //   int ndents=(hdr->BPB_SecPerClus*hdr->BPB_BytsPerSec)/sizeof(struct fat32dir);
-  //   fat32dir* dirs=ClusToDir(hdr,NextCluster);
-
-  //   for (int d = 0; d < ndents;d++){
-  //     if(dirs[d].DIR_Name[0]==0x00)
-  //         break;
-  //     if(dirs[d].DIR_Name[0]==0xe5||dirs[d].DIR_Attr==ATTR_LONG_NAME)
-  //         continue;
-  //     //打印name
-  //     printf("Short name: %s cnt:%d\n", dirs[d].DIR_Name,cnt++);
-  //   } }
-
-      printf("EntCnt:%d,cnt:%d\n", EntCnt, cnt);
-
 
   munmap(hdr, hdr->BPB_TotSec32 * hdr->BPB_BytsPerSec);
 }
