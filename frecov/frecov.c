55e1ef371ac205ff2075dbccc459870c6be9655b
tracer-nju <tracer@nju.edu.cn>
2024-04-24 15:36:41 +0800

    Linux niujh-Jiaolong-Series-GK5NP5O 5.15.0-94-generic #104~20.04.1-Ubuntu SMP Tue Jan 16 13:34:09 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:36:41 up 48 min,  1 user,  load average: 1.68, 1.30, 1.24

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -53,13 +53,13 @@ int main(int argc, char *argv[]) {
   int DataSec = TotSec - (hdr->BPB_RsvdSecCnt + (hdr->BPB_NumFATs * FATSz) +
                           RootDirSectors);
   int CountOfClusters = DataSec / hdr->BPB_SecPerClus;
-  u32 FirstDataSector = hdr->BPB_RsvdSecCnt + hdr->BPB_NumFATs * hdr->BPB_FATSz32 + fat32Info->RootDirSectors;
+  u32 FirstDataSector = hdr->BPB_RsvdSecCnt + hdr->BPB_NumFATs * hdr->BPB_FATSz32 + RootDirSectors;
   u32 FirstDirAddr = FirstDataSector * hdr->BPB_BytsPerSec;
   void* fstclusAddr=(char*)hdr+FirstDirAddr;
 
   fat32Info=&(struct fat32Info_t){.CountOfClusters=CountOfClusters,.DataSec=DataSec,
                                         .FATSz=FATSz,.RootDirSectors=RootDirSectors,
-                                        .TotSec=TotSec,.fstclusAddr=(void*)0};
+                                        .TotSec=TotSec,.fstclusAddr=fstclusAddr};
 
   printf("CountOfClusters: %d\n", CountOfClusters);
 
