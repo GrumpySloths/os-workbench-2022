248e93135bfe76da3959049b6e132ac32f344bdf
tracer-nju <tracer@nju.edu.cn>
2024-04-24 15:31:43 +0800

    Linux niujh-Jiaolong-Series-GK5NP5O 5.15.0-94-generic #104~20.04.1-Ubuntu SMP Tue Jan 16 13:34:09 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:31:43 up 43 min,  1 user,  load average: 1.15, 1.07, 1.19

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -53,9 +53,9 @@ int main(int argc, char *argv[]) {
   int DataSec = TotSec - (hdr->BPB_RsvdSecCnt + (hdr->BPB_NumFATs * FATSz) +
                           RootDirSectors);
   int CountOfClusters = DataSec / hdr->BPB_SecPerClus;
-  // u32 FirstDataSector = hdr->BPB_RsvdSecCnt + hdr->BPB_NumFATs * hdr->BPB_FATSz32 + fat32Info->RootDirSectors;
-  // u32 FirstDirAddr = FirstDataSector * hdr->BPB_BytsPerSec;
-  // char* fstclusAddr=(char*)hdr+FirstDirAddr;
+  u32 FirstDataSector = hdr->BPB_RsvdSecCnt + hdr->BPB_NumFATs * hdr->BPB_FATSz32 + fat32Info->RootDirSectors;
+  u32 FirstDirAddr = FirstDataSector * hdr->BPB_BytsPerSec;
+  char* fstclusAddr=(char*)hdr+FirstDirAddr;
 
   fat32Info=&(struct fat32Info_t){.CountOfClusters=CountOfClusters,.DataSec=DataSec,
                                         .FATSz=FATSz,.RootDirSectors=RootDirSectors,
