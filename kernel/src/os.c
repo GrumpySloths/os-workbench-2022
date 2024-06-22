db242bd5a54df00ead8b57fbeb51246c951f6553
tracer-nju <tracer@nju.edu.cn>
2024-06-22 11:32:57 +0800

    Linux niujh-Jiaolong-Series-GK5NP5O 5.15.0-94-generic #104~20.04.1-Ubuntu SMP Tue Jan 16 13:34:09 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:32:57 up  2:04,  1 user,  load average: 1.41, 1.08, 0.80

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -143,7 +143,8 @@ static Context* irq_syscall(Event ev,Context*ctx){
         ctx->rax = uproc->fork(current_task);
         break;
     case SYS_sleep:
-        ctx->rax =uproc->sleep(current_task,ctx->rdi);
+        printf("sleep ic called in syscall\n");
+        ctx->rax = uproc->sleep(current_task, ctx->rdi);
         // ctx->rax = 0;
         break;
     default:
