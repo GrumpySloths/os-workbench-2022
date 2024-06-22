531bab8d696d5f6c2c60fb3054e137b648397b3e
tracer-nju <tracer@nju.edu.cn>
2024-06-22 11:48:36 +0800

    Linux niujh-Jiaolong-Series-GK5NP5O 5.15.0-94-generic #104~20.04.1-Ubuntu SMP Tue Jan 16 13:34:09 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:48:36 up  2:20,  1 user,  load average: 1.20, 0.95, 0.82

--- kernel/src/uproc.c
+++ kernel/src/uproc.c
@@ -148,7 +148,7 @@ int uproc_sleep(task_t* task, int seconds) {
 
     while(io_read(AM_TIMER_UPTIME).us/1000-cur_time<seconds*1000);
 
-    printf("sleep is over\n");
+    // printf("sleep is over\n");
 
     return 0;
 }
