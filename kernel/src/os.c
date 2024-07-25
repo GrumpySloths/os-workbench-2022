642ae67ecc8cf3d58ce049ed1755bffbbfd77e19
tracer-nju <tracer@nju.edu.cn>
2024-07-25 10:05:54 +0800

    Linux niujh-Jiaolong-Series-GK5NP5O 5.15.0-94-generic #104~20.04.1-Ubuntu SMP Tue Jan 16 13:34:09 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:05:54 up  1:37,  3 users,  load average: 1.09, 0.81, 0.71

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -280,10 +280,10 @@ static void os_init() {
 #else
   uproc->init();
 #endif
-
-    //构建轮询链表
-    for (int i = 0; i < tasks_id;i++){
-      tasks[i]->next=tasks[(i+1)%tasks_id];
+    uproc->init();
+    // 构建轮询链表
+    for (int i = 0; i < tasks_id; i++) {
+      tasks[i]->next = tasks[(i + 1) % tasks_id];
     } 
 
 
